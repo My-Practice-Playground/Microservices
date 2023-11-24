4cc984ba4239012371aef8da578b11f4f493254a
Lakshan-Chamoditha-Perera <perera.alc2000@gmail.com>
2023-11-24 10:40:00 +0530

    service layer

---
+++ user_service/src/main/java/com/micro_services/user_service/service/UserService.java
@@ -0,0 +1,11 @@
+package com.micro_services.user_service.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Author: shan
+ * Created: 2023-11-24 10.38
+ */
+
+public interface UserService {
+}
---
+++ user_service/src/main/java/com/micro_services/user_service/service/custom/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.micro_services.user_service.service.custom;
+
+import com.micro_services.user_service.service.UserService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Author: shan
+ * Created: 2023-11-24 10.38
+ */
+@Service
+public class UserServiceImpl implements UserService {
+}

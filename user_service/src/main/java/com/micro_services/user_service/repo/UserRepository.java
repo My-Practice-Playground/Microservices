1494fa693b62263b10332e4ace3f99f12bf9dfe8
Lakshan-Chamoditha-Perera <perera.alc2000@gmail.com>
2023-11-24 10:41:15 +0530

    repository layer

---
+++ user_service/src/main/java/com/micro_services/user_service/repo/UserRepository.java
@@ -0,0 +1,17 @@
+package com.micro_services.user_service.repo;
+
+import com.micro_services.user_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Author: shan
+ * Created: 2023-11-24 10.40
+ */
+
+@EnableJpaRepositories
+@Repository
+public interface UserRepository extends JpaRepository<User,String> {
+}

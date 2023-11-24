54251d8697a3475850bdc376da1c1299c760a031
Lakshan-Chamoditha-Perera <perera.alc2000@gmail.com>
2023-11-24 10:38:06 +0530

    api layer

---
+++ user_service/src/main/java/com/micro_services/user_service/api/UserController.java
@@ -0,0 +1,12 @@
+package com.micro_services.user_service.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author: shan
+ * Created: 2023-11-24 10.37
+ */
+@RestController
+public class UserController {
+
+}

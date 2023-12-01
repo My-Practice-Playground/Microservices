2a3f9be20ac4ce1d3fa870ac7339b48342272352
Lakshan-Chamoditha-Perera <perera.alc2000@gmail.com>
2023-12-01 10:00:24 +0530

    user server configured in configuration server

--- config-server/src/main/java/com/micro_services/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/micro_services/configserver/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.micro_services.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer //this annotation is used to enable config server
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
